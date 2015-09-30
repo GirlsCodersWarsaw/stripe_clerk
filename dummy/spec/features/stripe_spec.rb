238bcf03f4b90ad1790a836c4d4b2d4e57aa5ac7
Basia Klosowska <barbaraklosowska@gmail.com>
2015-09-30 16:11:49 +0200

    change stripe_spec

--- dummy/spec/features/stripe_spec.rb
+++ dummy/spec/features/stripe_spec.rb
@@ -2,30 +2,12 @@ require 'spec_helper'
 require 'office_clerk'
 
 describe "pay with stripe", :js => true do
-  # it "works with sign in" do
-  #   clerk = sign_in
-  #   order = create :order, email: clerk.email
-  #   page.set_rack_session order: order.id
-  #   page.visit shop_order_path
-  #   click_button 'Pay with Card'
-  #   stripe_iframe = all('iframe[name=stripe_checkout_app]').last
-  #   Capybara.within_frame stripe_iframe do
-  #     fill_in "email", :with => 'user@example.com'
-  #     fill_in "card_number", :with => '4242424242424242'
-  #     fill_in "cc-exp", :with => '11/15'
-  #     fill_in "cc-csc", :with => '123'
-  #     click_button "Pay"
-  #   end
-  #   sleep(7)
-  #   Capybara.click_link('/history')
-  #   ensure_path shop_history_path
-  #   expect(page).to have_content("Thanks, you paid")
-  # end
-  it "works without sign in" do
+  before do
     order = create :order
     page.set_rack_session order: order.id
     page.visit shop_order_path
-
+  end
+  it "works" do
     click_button 'Pay with Card'
     stripe_iframe = all('iframe[name=stripe_checkout_app]').last
     Capybara.within_frame stripe_iframe do
@@ -36,5 +18,18 @@ describe "pay with stripe", :js => true do
       click_button "Pay"
     end
     expect(page).to have_content("Thanks, you paid")
+  end
+
+  it "has errors" do
+    click_button 'Pay with Card'
+    stripe_iframe = all('iframe[name=stripe_checkout_app]').last
+    Capybara.within_frame stripe_iframe do
+      fill_in "email", :with => 'user@example.com'
+      fill_in "card_number", :with => '4000000000000002'
+      fill_in "cc-exp", :with => '11/20'
+      fill_in "cc-csc", :with => '123'
+      click_button "Pay"
+      expect(page).to have_content("This card was declined")
+    end
   end
 end
